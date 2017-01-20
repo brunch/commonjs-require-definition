a908db85b8a24734a7a4d4a88f14369e723dfa12
Paul Miller <paul@paulmillr.com>
2017-01-20 04:11:08 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+'use strict';
 const readFile = require('fs').readFileSync;
+const join = require('path').join;
 
-module.exports = readFile('require.js', 'utf8');
+module.exports = readFile(join(__dirname, 'require.js'), 'utf8');
